c0d8dcd9c812f96d5a0e2087deb7de89babc50c9
shubham <shubhshri25gmail.com>
2024-08-09 20:08:56 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+
+app.listen(port, () => console.log(`server started at port : ${port}`));
