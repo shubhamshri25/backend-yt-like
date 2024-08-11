9ac17a49e48e959a6b803d2a176b2f2eecaaa363
shubham <shubhshri25gmail.com>
2024-08-11 13:52:57 +0530

    connected mongo altad

---
+++ src/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const DB_NAME = require("./constant");
+
+mongoose
+  .connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("DB error", err));
+
+const db = mongoose.connection;
+
+module.exports = db;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const app = express()
-require('dotenv').config();
-const port = process.env.PORT || 3000
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const db = require("../src/db");
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Listening on port ${port}!`));
